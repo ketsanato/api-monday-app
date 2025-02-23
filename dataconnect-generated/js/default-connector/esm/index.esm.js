cdf2238fc314c81ae6d5b1a5bacb4bea3f9c7ea2
ketsana <54395017+ketsanato@users.noreply.github.com>
2025-02-23 21:19:00 +0700

    Add files via upload

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { , validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'web-api-server-monday-main',
+  location: 'us-central1'
+};
+
