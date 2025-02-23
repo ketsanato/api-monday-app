9d262cf64d7a13c9bbec1c2b8ee454d0275f4af5
ketsana <54395017+ketsanato@users.noreply.github.com>
2025-02-23 21:24:20 +0700

    Add files via upload

---
+++ contorllers/users1.contorllers.js
@@ -0,0 +1,227 @@
+const userModels = require("../models/users1.models");
+const jwt = require('jsonwebtoken');
+const maxAge = 60000;
+const bcrypt = require('bcryptjs');
+const OTPGenerator = require('otp-generator');
+
+
+module.exports.createUsers = async (req, res, next) => {
+
+    const form = req.body;
+    const salt = await bcrypt.genSalt();
+    password = await bcrypt.hash(form.Password, salt);
+
+    const data = {
+        username: form.Username,
+        password: password,
+        user_online: false,
+        Tel:form.Tel,
+        user_type_id: form.user_type_id,
+        created_date: new Date()
+    }
+
+    console.log(data)
+    
+    await userModels.create(data, err => {
+
+        if (!err) {
+
+
+            console.log("Save");
+            res.json({
+                status: true,
+                message: "Saved"
+
+            })
+        } else {
+            console.log("error ");
+
+            res.json({
+                status: false,
+                message:err
+
+
+            })
+        }
+    })
+
+
+
+
+
+
+
+
+};
+
+
+module.exports.readUsers1 = async (req, res, next) => {
+
+    userModels.aggregate([{
+        $lookup: {
+            from: "user_types",
+            localField: "user_type_id",
+            foreignField: "_id",
+            as: "user_type_name"
+        }
+    }, ]).exec((err, data) => {
+
+        if (!err) {
+
+            res.json({
+                status: true,
+                message: "selete all data ",
+                data: data
+            })
+
+        } else {
+
+
+            console.log("error");
+            res.json({
+                status: false,
+                message: err
+            })
+        }
+    });
+
+}
+
+module.exports.readUsers = async (req, res, next) => {
+
+    userModels.find().exec((err, data) => {
+
+        if (!err) {
+
+
+            res.json({
+                status: true,
+                message: "selete all data ",
+                data: data
+            })
+        } else {
+
+
+
+            res.json({
+                status: false,
+                message: err
+            })
+        }
+    });
+
+}
+
+module.exports.testUser = async (req, res, next) => {
+
+    userModels.find().populate('author').exec((err, data) => {
+
+        if (!err) {
+
+
+            res.json({
+                status: true,
+                message: "selete all data ",
+                data: data
+            })
+        } else {
+
+
+            console.log("error");
+            res.json({
+                status: false,
+                message: err
+            })
+        }
+    });
+
+}
+
+
+module.exports.updateUsers = async (req, res, next) => {
+
+    const form = req.body;
+    const salt = await bcrypt.genSalt();
+    password = await bcrypt.hash(form.Password, salt);
+
+    const data = {
+        username: form.Username,
+        password: password,
+        user_online: false,
+        Tel:form.Tel,
+        user_type_id: form.user_type_id,
+        updated_date: new Date()
+        
+    }
+    userModels.findByIdAndUpdate(form._id, data, {
+        useFindAndModify: false
+    }).exec((err, data) => {
+
+        if (!err) {
+
+            res.json({
+                status: true,
+                message: "Update Sucess !",
+                data: data
+
+            })
+
+        } else {
+            console.log("error");
+
+            res.json({
+                status: false,
+                message: err
+            })
+
+
+        }
+    })
+
+
+
+}
+
+module.exports.deleteUsers = async (req, res, next) => {
+
+    const form = req.body;
+
+
+    userModels.findByIdAndDelete(form._id, {
+        useFindAndModify: false
+    }).exec((err, data) => {
+
+        if (!err) {
+            console.log("Delete Sucess  ");
+
+            res.json({
+                status: true,
+                message: "Delete Sucess !",
+                data: data
+
+            })
+
+        } else {
+
+            console.log(" Delete error");
+
+            res.json({
+                status: false,
+                message: err
+            })
+
+
+        }
+    })
+
+
+
+}
+
+module.exports.SendOTP = async (req, res, next) => {
+
+    const form = req.body;
+
+
+ 
+}
